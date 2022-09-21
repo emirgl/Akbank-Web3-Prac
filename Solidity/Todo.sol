bff1d6af5c900bb0bc597e2cb74a86162370fd57
Emir Gulcemal <69427895+emirgl@users.noreply.github.com>
2022-09-21 18:13:55 +0300

    Update Todo.sol

--- Solidity/Todo.sol
+++ Solidity/Todo.sol
@@ -7,25 +7,25 @@ contract TodoList{
         bool completed;
     }
 
-    Todo[] public todos;
+    Todo[] public todos;   // array list 
 
-    function setNote(string calldata _note) external {
+    function setNote(string calldata _note) external {     // create a new Note
         todos.push(Todo({
             note: _note,
             completed: false
         }));
     }
 
-    function updateNote(uint _index , string calldata _note) external {
+    function updateNote(uint _index , string calldata _note) external {    // update Notes on list 
         todos[_index].note = _note;
     }
 
-    function getNote(uint _index) external view returns (string memory, bool){
+    function getNote(uint _index) external view returns (string memory, bool){    // get Note with index 
         Todo memory todo = todos[_index];
         return (todo.note, todo.completed);
     }
 
-    function markAsCompleted(uint _index) external {
+    function markAsCompleted(uint _index) external {       // complete the Note
         todos[_index].completed = true;
     }
 }
